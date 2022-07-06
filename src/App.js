01091cfa3ecc071f5ef0dd0fa5857f52dccdb260
Mityco <nice.materov@mail.ru>
2022-07-06 19:21:52 +0400

    Создан каркас основной страницы

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
+import './Button.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
-    </div>
+          <body className="App-body">
+              <table align="center" cellSpacing="15">
+                  <tr>
+                      <td> <img src={logo} className="App-logo" alt="logo" /> </td>
+                      <td>
+                          <h1>Star Wars</h1>
+                      </td>
+
+                  </tr>
+                  <tr>
+                      <td> <button type="Button" className="btn">People</button> </td>
+                      <td> <button type="Button" className="btn">Starships</button> </td>
+                      <td> <button type="Button" className="btn">Vehicles</button> </td>
+                  </tr>
+                  <tr>
+                      <td> <button type="Button" className="btn">Planets</button> </td>
+                      <td> <button type="Button" className="btn">Species</button> </td>
+                      <td> <button type="Button" className="btn">Films</button> </td>
+                  </tr>
+              </table>
+          </body>
+      </div>
   );
 }
 
